a71210e680d3599b38ae8a476a78d1d1bd87a678
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-21 23:45:31 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,8 @@ class StudentsController < ApplicationController
     set_student
     @student.activate ? @student.active = false : @student.active = true
     @student.save
+
+    redirect_to student_path(@student)
   end
 
   private
